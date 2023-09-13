cf9c87ccc93b3ed906db6f4fa239364017f3f210
Ajayydev <136185490+Ajibuah@users.noreply.github.com>
2023-09-13 10:42:44 +0200

    Update Assignment-guess-numbers.py

--- Assignment-guess-numbers.py
+++ Assignment-guess-numbers.py
@@ -1,19 +1,19 @@
 import random
 
-randomNumbers = random.randint(1,100)
+randomNumbers = random.randint(1, 100)
 userChoice = 0
-
+chance = 5
+print("The number guessing game")
+print(f"You have the {chance} to guess the number correctly from 1-100")
 while userChoice != randomNumbers:
     userChoice = int(input("Guess the First number in my mind: "))
-    userchoice_a = int(input("Guess the Second number in my mind: "))
-    userchoice_b = int(input("Guess the Third number in my mind: "))
-    userchoice_c = int(input("Guess the Fourth number in my mind: "))
-    userchoice_d = int(input("Guess the Fifth24 number in my mind: "))
 
     if userChoice < randomNumbers:
         print("Your choice is too low!")
 
     elif userChoice > randomNumbers:
         print("Your choice is too high!")
-
-print("You've quessed correcty!")
+if userChoice == randomNumbers:
+    print(f"You've guessed correctly! The number was {randomNumbers}.")
+else:
+    print("You've quessed correcty!")
